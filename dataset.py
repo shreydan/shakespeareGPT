8b7e37d2838166770a802f90ecdb2eae3b08a97f
shreydan <iamshreydan@gmail.com>
2023-02-13 23:49:34 +0530

    better dataset

--- dataset.py
+++ dataset.py
@@ -1,16 +1,27 @@
 from tokenizers import Tokenizer
 import torch
 from pathlib import Path
+from dataclasses import dataclass
+
 
 class ShakespeareDataset:
-    def __init__(self,block_size:int) -> None:
+    def __init__(self, Config, is_test=False) -> None:
         self.file_path = Path('./data/shakespeare.txt')
         self.tokenizer_path = Path('./tokenizer/shakespeare.json')
         with open(self.file_path,'r',encoding='utf-8') as f:
             self.data = f.read()
         self.tokenizer = Tokenizer.from_file(str(self.tokenizer_path))
-        self.data = self.tokenizer.encode(self.data).ids
-        self.block_size = block_size
+        
+        self.full_data = torch.tensor(self.tokenizer.encode(self.data).ids, dtype=torch.long)
+
+        self.is_test = is_test
+        if self.is_test:
+            self.data = self.full_data[int(0.9*len(self.full_data)):]
+        else:
+            self.data = self.full_data[:int(0.9*len(self.full_data))]
+
+        self.block_size = Config.block_size
+        self.batch_size = Config.batch_size
 
     def __len__(self) -> int:
         return len(self.data)
@@ -21,18 +32,24 @@ class ShakespeareDataset:
     def get_vocab_size(self) -> int:
         return self.tokenizer.get_vocab_size()
 
-    def __getitem__(self,idx):
-        item = self.data[idx:idx+self.block_size+1]
-        x = torch.tensor(item[:-1],dtype=torch.long)
-        y = torch.tensor(item[1:],dtype=torch.long)
+    def __next__(self):
+        ix = torch.randint(len(self.data) - self.block_size, (self.batch_size,))
+        x = torch.stack([self.data[i:i+self.block_size] for i in ix])
+        y = torch.stack([self.data[i+1:i+self.block_size+1] for i in ix])
         return x,y
+
+
 
     
 if __name__ == '__main__':
-    dataset = ShakespeareDataset(block_size=8)
-    print(len(dataset))
-    print(dataset[0])
-    print(dataset.get_block_size())
-    print(dataset.get_vocab_size())
-    dl = torch.utils.data.DataLoader(dataset,shuffle=False,batch_size=4)
-    print(next(iter(dl)))
+
+    @dataclass
+    class Config:
+        batch_size = 4
+        block_size = 8
+        
+    ds = ShakespeareDataset(Config)
+    for i in range(4):
+        x,y = next(ds)
+        print(x.shape,y.shape)
+    
