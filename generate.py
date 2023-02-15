cfdcb693ee39e1afca936044f86a1b4995551e4e
shreydan <iamshreydan@gmail.com>
2023-02-15 13:08:20 +0530

    generate on CPU

--- generate.py
+++ generate.py
@@ -1,19 +1,40 @@
 import torch
 from gpt import ShakespeareGPT
-from train import Config
 from tokenizers import Tokenizer
+from dataclasses import dataclass
 
 model_path = ''
 
 tokenizer = Tokenizer.from_file('./tokenizer/shakespeare.json')
 
+@dataclass
+class Config:
+    
+    block_size = 256 # context-length
+    batch_size = 64 # mini-batch size
+    
+    vocab_size = tokenizer.get_vocab_size()
+    
+    n_embed = 384
+    n_heads = 12
+    head_size = n_embed // n_heads # computes to 384/12=32
+    
+    n_layers = 4
+    
+    attn_dropout = 0.1
+    block_dropout = 0.1
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+print(Config.device)
 
 lm = ShakespeareGPT(Config)
 state_dict = torch.load(model_path, map_location='cpu')
 lm.load_state_dict(state_dict)
 
 generated_texts = []
-for length in [100,300,500,700,1000]:
+for length in [100]:
     generated = lm.generate(
     torch.zeros((1,1),dtype=torch.long,device='cpu'), # initial context 0
     total=length
