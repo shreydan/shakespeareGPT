ec5891b913ad28f578662e804cb7cea747ee4a29
shreydan <iamshreydan@gmail.com>
2023-02-15 12:57:21 +0530

    shakespeareGPT model

---
+++ gpt.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AttentionHead(nn.Module):
+    def __init__(self, Config):
+        super().__init__()
+
+        self.block_size = Config.block_size
+        self.n_embed = Config.n_embed
+        self.head_size = Config.head_size
+        
+        self.key = nn.Linear(self.n_embed, self.head_size, bias=False)
+        self.query = nn.Linear(self.n_embed, self.head_size, bias=False)
+        
+        self.value = nn.Linear(self.n_embed, self.head_size, bias=False)
+
+        self.register_buffer(
+            'tril',
+            torch.tril(torch.ones(self.block_size,self.block_size))
+        )
+        
+        self.dropout = nn.Dropout(Config.attn_dropout)
+
+    def forward(self, x):
+
+        B,T,C = x.shape
+
+        k = self.key(x)
+        q = self.query(x)
+
+        wei = q@k.transpose(-2,-1) * (C ** 0.5)
+        wei = wei.masked_fill(self.tril[:T,:T]==0,float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        
+        v = self.value(x)
+        out = wei @ v
+        
+        return out
+
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, Config):
+        super().__init__()
+        self.n_heads = Config.n_heads
+        self.head_size = Config.head_size
+        
+        self.heads = nn.ModuleList([AttentionHead(Config) for _ in range(self.n_heads)])
+        
+        self.projection = nn.Linear(Config.n_embed, Config.n_embed)
+        
+        self.dropout = nn.Dropout(Config.attn_dropout)
+    
+    def forward(self,x):
+        x = torch.cat([h(x) for h in self.heads],dim=-1)
+        x = self.projection(x)
+        x = self.dropout(x)
+        return x
+
+
+
+class FeedForward(nn.Module):
+    def __init__(self, Config):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(Config.n_embed,Config.n_embed * 4),
+            nn.ReLU(),
+            nn.Linear(Config.n_embed * 4, Config.n_embed), # projection
+            nn.Dropout(Config.block_dropout)
+        )
+    def forward(self,x):
+        return self.net(x)
+
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, Config):
+        super().__init__()
+        self.attn = MultiHeadAttention(Config)
+        self.ff = FeedForward(Config)
+        self.ln1 = nn.LayerNorm(Config.n_embed)
+        self.ln2 = nn.LayerNorm(Config.n_embed)
+
+    def forward(self,x):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.ff(self.ln2(x))
+        
+        return x
+
+
+
+class ShakespeareGPT(nn.Module):
+    def __init__(self,Config):
+        super().__init__()
+        
+        self.n_embed = Config.n_embed
+        self.block_size = Config.block_size
+        
+        self.token_embedding_table = nn.Embedding(Config.vocab_size,self.n_embed)
+        self.pos_embedding_table = nn.Embedding(self.block_size, self.n_embed)
+        
+        self.blocks = nn.Sequential(
+            *[TransformerBlock(Config)]*Config.n_layers,
+            nn.LayerNorm(self.n_embed)
+        )
+
+        self.lm_head = nn.Linear(self.n_embed,Config.vocab_size)
+        
+    def forward(self,idx):
+        
+        B,T = idx.shape
+        
+        token_embs = self.token_embedding_table(idx)
+        pos_embs = self.pos_embedding_table(torch.arange(T,device=Config.device))
+        
+        
+        x = token_embs + pos_embs
+        x = self.blocks(x)
+        logits = self.lm_head(x)
+        
+        return logits
+
+        
+    def generate(self,idx,total):
+        for _ in range(total):
+            idx_cond = idx[:, -self.block_size:]
+            logits= self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+            
