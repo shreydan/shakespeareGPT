4b8ca46926f43c2ff2263f8bc399bf30ad63abd1
shreydan <iamshreydan@gmail.com>
2023-02-15 12:31:44 +0530

    update tokenizer as per GPT2

--- build_tokenizer.py
+++ build_tokenizer.py
@@ -1,22 +1,22 @@
 from pathlib import Path
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
-from tokenizers.pre_tokenizers import ByteLevel
+import tokenizers.pre_tokenizers as pre_tokenizers
 import tokenizers.processors as processors
 import tokenizers.decoders as decoders
 from tokenizers.trainers import BpeTrainer
-from tokenizers.normalizers import NFKC
 
 if __name__ == '__main__':
 
     tokenizer_path = Path('./tokenizer/')
     tokenizer_path.mkdir(exist_ok=True)
 
-    tokenizer = Tokenizer(BPE(unk_token='<|unknown|>'))
-    tokenizer.pre_tokenizer = ByteLevel(add_prefix_space=True)
-    tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+    tokenizer = Tokenizer(BPE())
+
+    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+    tokenizer.post_processor = processors.ByteLevel(trim_offsets=False)
     tokenizer.decoder = decoders.ByteLevel()
-    tokenizer.normalizer = NFKC()
-    trainer = BpeTrainer(special_tokens=['<|unknown|>'], min_frequency=2)
+    trainer = BpeTrainer(special_tokens=['<|endoftext|>'], min_frequency=2)
+
     tokenizer.train(['./data/shakespeare.txt'],trainer)
     tokenizer.save(str(tokenizer_path / 'shakespeare.json'))
